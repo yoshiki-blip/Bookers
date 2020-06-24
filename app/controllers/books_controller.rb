f1cb6fa063cea961b12cae3ac8226c34d56741e4
vagrant <vagrant@localhost.localdomain>
2020-06-24 00:59:52 +0000

    Bookers-first

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def index
+  	@books=Book.all
+  	@book=Book.new
+  end
+
+  def show
+  	@book=Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  	@book=Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "error"
+      @books=Book.all
+      render :index
+    end
+  end
+
+  def edit
+  	@book=Book.find(params[:id])
+  end
+
+  def destroy
+  	 book = Book.find(params[:id])
+     book.destroy
+     redirect_to books_path
+  end
+
+  def update
+  	book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title,:body)
+  end
+end
